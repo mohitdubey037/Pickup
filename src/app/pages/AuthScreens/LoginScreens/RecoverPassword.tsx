2d73b506edbaf937cc9a15d81c1b69c53da4da7e
Akshay patil <akshay.patil@torinit.ca>
2021-09-21 15:59:24 +0530

    formik validation

--- src/app/pages/AuthScreens/LoginScreens/RecoverPassword.tsx
+++ src/app/pages/AuthScreens/LoginScreens/RecoverPassword.tsx
@@ -1,6 +1,8 @@
 import { RouteComponentProps } from "@reach/router";
+import { useFormik } from "formik";
 import { Button } from "../../../components/Buttons";
 import { PasswordInput } from "../../../components/Input";
+import { useDispatch, useSelector } from "react-redux";
 import {
   FormContent,
   FormWrapper,
@@ -8,17 +10,51 @@ import {
   Header,
   LoginWrapper,
 } from "../style";
+import { recoverPasswordSchema } from "./recoverPasswordSchema";
+import {
+  registerUser,
+  setRegisterUserResponse,
+} from "store/reducers/actions/signUpActions";
 
 const RecoverPassword = ({ navigate }: RouteComponentProps) => {
+  const dispatch = useDispatch();
+  const Confirm = () => {};
+  const {
+    handleChange,
+    values: { password },
+    errors,
+    touched,
+    handleBlur,
+    handleSubmit,
+  } = useFormik({
+    initialValues: { password: "", confirmpassword: "" },
+    validationSchema: recoverPasswordSchema,
+    onSubmit: Confirm,
+  });
   return (
     <LoginWrapper>
       <LogoImage />
       <FormWrapper>
         <FormContent>
           <Header>RECOVER PASSWORD</Header>
-          <PasswordInput label="Password" placeholder="Start typing" validate/>
-          <PasswordInput label="Confirm Password" placeholder="Start typing" />
-          <Button label="Confirm" onClick={() => {}} />
+          <PasswordInput
+            id="password"
+            name="password"
+            label="Password"
+            error={ errors.password}
+            onChange={handleChange}
+            placeholder="Start typing"
+            validate
+          />
+          <PasswordInput
+            id="confirmpassword"
+            name="confirmpassword"
+            error={ errors.confirmpassword}
+            onChange={handleChange}
+            label="Confirm Password"
+            placeholder="Start typing"
+          />
+          <Button label="Confirm" onClick={handleSubmit} />
         </FormContent>
       </FormWrapper>
     </LoginWrapper>
