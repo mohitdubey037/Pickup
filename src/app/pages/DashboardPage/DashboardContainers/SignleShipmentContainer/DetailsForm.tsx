67e3b137ab778d52e0ffe8d19fa173a6a1e75095
akshay-patil-torinit <akshay.patil@torinit.ca>
2021-12-21 16:11:39 -0800

    changes made in API

--- src/app/pages/DashboardPage/DashboardContainers/SignleShipmentContainer/DetailsForm.tsx
+++ src/app/pages/DashboardPage/DashboardContainers/SignleShipmentContainer/DetailsForm.tsx
@@ -5,10 +5,48 @@ import { Flex } from "app/components/Input/style";
 import RadioGroup from "app/components/RadioGroup";
 import Select from "app/components/Select";
 import { CustomInput } from "../CompanyProfileContainer/style";
+import { useEffect, useState } from "react";
+import { getCategoryList } from "services/SingleShipmentServices";
 
+interface SelectBoardType{
+  categoryId:number,
+  createdAt:string,
+  createdBy:number,
+  description:string,
+  icon:string,
+  name:string,
+  selectedIcon:string,
+  status:number,
+  typeId:number,
+  updatedAt:string,
+  updatedBy:number
+}
+interface SelectCategoryType{
+  id?:number,
+  name?:string,
+  value:string |number
+}
 function DetailsForm(props: { formik: FormikValues }) {
   const { handleChange, errors, touched, handleBlur, handleSubmit } =
     props.formik;
+    const [selectedBoard, setSelectedBoard] = useState<SelectBoardType[]>([]);
+  const [category, setCategory] =  useState<SelectCategoryType>()
+  const loadCategory = async ()=>{
+   
+ 
+    
+  }
+  useEffect(() => {
+    (async () => { 
+      const response = await getCategoryList();
+      if (response.success&& response.response){
+        const data = response.response as {data: any}
+        console.log(data.data.data,"its the response")
+         setSelectedBoard(data.data.data)
+      }
+    })()
+    
+  }, [])
   return (
     <>
       <Typography className="typography" variant="h1" component="h3">
@@ -21,7 +59,11 @@ function DetailsForm(props: { formik: FormikValues }) {
               id="Category"
               name="Category"
               label={"Category"}
+              value={category?.value}
+              onSelect={({target:{value,id,name}})=>{setCategory({value,id,name})}}
+              options={selectedBoard ? selectedBoard.map(option => ({value: option.categoryId, label: option.name})):[]}
             />
+        
           </Flex>
           <Flex flex={1} left={30}>
             <Select label={"Customer Reference Number"} />
