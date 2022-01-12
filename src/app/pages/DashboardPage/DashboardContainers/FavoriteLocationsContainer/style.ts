c41e9db4131852287ff02110f707abf7f83b906c
akshay-patil-torinit <akshay.patil@torinit.ca>
2022-01-12 16:20:03 +0530

    drawer added  with file drop functionality

--- src/app/pages/DashboardPage/DashboardContainers/FavoriteLocationsContainer/style.ts
+++ src/app/pages/DashboardPage/DashboardContainers/FavoriteLocationsContainer/style.ts
@@ -3,3 +3,55 @@ import styled from "styled-components";
 export const test = styled.div`
     color: red
 `
+
+export const UploadingWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width:fit-content;
+  paddingLeft:100;
+  
+`
+
+export const DropText = styled.p`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  text-align: center;
+  color: #a6a6a6;
+  a {
+    text-decoration: underline;
+    color: #1b8af0;
+  }
+`;
+export const HelperText = styled.div`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 14px;
+  text-align: center;
+  color: #c1d4d7;
+`;
+export const Wrapper = styled.div`
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  gap:10px;
+`;
+export const DropzoneBox = styled.div`
+  width: 100%;
+  height: 289px;
+  border: 1.5px dashed #c1d4d7;
+  box-sizing: border-box;
+  border-radius: 8.71486px;
+  display: flex;
+  flex-direction: center;
+  justify-content: center;
+  align-items: center;
+  section {
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+  }
+`;
