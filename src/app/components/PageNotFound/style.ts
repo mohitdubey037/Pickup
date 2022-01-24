4cbdf3d361ec0a71801fcc65735e5f3e1ca59d8a
tushar9sd <tushar@torinit.ca>
2022-01-24 18:23:00 +0530

    component added

---
+++ src/app/components/PageNotFound/style.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const PageNotFoundBox = styled.div`
+  display: flex;
+  align-items:Center;
+  justify-content:center;
+  height:100vh;
+  align-items:center;
+  justify-content:center;
+  flex-direction: column;
+  .backBtn{
+      margin-top:50px;
+  }
+`;
+
+
+export const PageNotFoundHeading = styled.div`
+  font-size:70px;
+  color:#222;
+  font-family: "Roboto-Bold";
+  margin-bottom:20px
+`;
+
+export const PageNotFoundSubHeading = styled.div`
+  font-size:30px;
+  color:#666;
+  font-family: "Roboto-Medium";
+  text-transform:capitalize;
+  letter-spacing: 1.5px;
+`;
+
+
+
