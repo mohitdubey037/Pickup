31820946a45fb1ee29f0c3979de48ee247861acd
akshay-patil-torinit <akshay.patil@torinit.ca>
2022-01-28 19:49:42 +0530

    placeholder for personal profile

--- src/app/pages/DashboardPage/DashboardContainers/PersonalProfileContainer/passwordSchema.ts
+++ src/app/pages/DashboardPage/DashboardContainers/PersonalProfileContainer/passwordSchema.ts
@@ -6,18 +6,18 @@ export const passwordSchema = yup.object().shape({
   currentPassword: yup
     .string()
     .matches(PASSWORD_REGX, "Invalid Password")
-    .required("Current Password is a required field"),
+    .required("Current password is a required field"),
   newPassword: yup
     .string()
     .matches(PASSWORD_REGX, "Invalid Password")
-    .required("New Password is required")
+    .required("New password is required field")
     .test("newPassword", "Please enter new password", function (value) {
       return this.parent.currentPassword !== value;
     }),
   newConfirmedPassword: yup
     .string()
     .matches(PASSWORD_REGX, "Invalid Password")
-    .required("New Confirm Password is required")
+    .required("Confirm password is required field")
     .oneOf(
       [yup.ref("newPassword"), null],
       "New Password and Confirm Password must match"
