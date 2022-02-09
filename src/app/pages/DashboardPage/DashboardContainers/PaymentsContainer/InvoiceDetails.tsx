466a6f7d544baa3da93cd168d050281479fd09ab
torinit-darshan <darshan.sudake@torinit.ca>
2022-02-09 13:39:51 +0530

    [PICKUPS-91] Shipment Summary - Terms and conditions open modal with desired content

--- src/app/pages/DashboardPage/DashboardContainers/PaymentsContainer/InvoiceDetails.tsx
+++ src/app/pages/DashboardPage/DashboardContainers/PaymentsContainer/InvoiceDetails.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Box } from "@mui/system";
+
 import { InsuranceIcon } from "app/assets/Icons";
 import { Checkbox } from "app/components/Checkbox";
 import { Flex } from "app/components/Input/style";
 import { Link } from "app/components/Link";
 import { H3, H4 } from "app/components/Typography/Typography";
+import TermsAndPolicies from "app/pages/AuthScreens/SignUpScreens/Terms&Policies";
 import { Termslink } from "app/pages/AuthScreens/style";
 
 const InvoiceDetails = ({ invoiceData, insuranceHandler }) => {
+    const [showTermsPolicies, setShowTermsPolicies] = useState("");
+
     return (
         <Box>
             <H3 text="Shipment Summary" />
@@ -35,15 +41,40 @@ const InvoiceDetails = ({ invoiceData, insuranceHandler }) => {
                         label="Check our Terms & Conditions"
                         style={{ color: "#1B8AF0" }}
                     /> */}
-                    <Termslink ml={4} style={{color: "#1B8AF0"}}>Check our <Link to="">Terms</Link> & <Link to="" >Conditions</Link></Termslink>
+                    <Termslink ml={4} style={{ color: "#1B8AF0" }}>
+                        Check our{" "}
+                        <Link
+                            to=""
+                            onClick={() => setShowTermsPolicies("terms")}
+                        >
+                            Terms
+                        </Link>{" "}
+                        &{" "}
+                        <Link
+                            to=""
+                            onClick={() => setShowTermsPolicies("policies")}
+                        >
+                            Conditions
+                        </Link>
+                    </Termslink>
                 </Flex>
 
                 <Flex justifyContent="space-between" top={40} bottom={16}>
                     <H4 text="Total" fontFamily="bold" />
-                    <H4 text={`$${Number(invoiceData.total).toFixed(2)}`} fontFamily="bold" />
+                    <H4
+                        text={`$${Number(invoiceData.total).toFixed(2)}`}
+                        fontFamily="bold"
+                    />
                 </Flex>
                 <hr />
             </Box>
+
+            <Modal
+                isOpen={!!showTermsPolicies}
+                onRequestClose={() => setShowTermsPolicies("")}
+            >
+                <TermsAndPolicies name={showTermsPolicies} />
+            </Modal>
         </Box>
     );
 };
