d4e557160fd84a1feb963aaaa07863421ce71c3b
torinit-darshan <darshan.sudake@torinit.ca>
2022-02-16 13:08:24 +0530

    [PICKUPS-185][PICKUPS-196] Search Order - search, sort and pagination functionalty

--- src/app/pages/DashboardPage/DashboardContainers/SearchContainer/SearchContainer.tsx
+++ src/app/pages/DashboardPage/DashboardContainers/SearchContainer/SearchContainer.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable no-debugger */
 import { useState, useEffect } from "react";
 import { useFormik } from "formik";
-import { useDispatch, useSelector } from "react-redux";
-import { navigate } from "@reach/router";
+import { useDispatch } from "react-redux";
 import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import moment from "moment";
@@ -10,7 +9,7 @@ import moment from "moment";
 import { Button } from "app/components/Buttons";
 import { Input } from "app/components/Input";
 import ModuleContainer from "app/components/ModuleContainer";
-import { Table } from "app/components/Table";
+import { TableNew } from "app/components/Table";
 import { H2, H3 } from "app/components/Typography/Typography";
 import { sliders } from "app/assets/Icons";
 import { Drawer } from "app/components/Drawer";
@@ -19,7 +18,7 @@ import DatePickerInput from "app/components/Input/DatePickerInput";
 import TableSkeleton from "app/components/Table/TableSkeleton";
 import NullState from "app/components/NullState/NullState";
 import SelectNew from "app/components/Select/SelectNew";
-import { searchTable } from "./helper";
+import { getSearchOrderData, searchOrderColoumns } from "./helper";
 import { SearchTableTop } from "./style";
 import AddNewPaymentDrawer from "../PaymentsContainer/AddNewPaymentDrawer";
 import AdvanceFilters from "./AdvanceFilters";
@@ -33,124 +32,36 @@ import { STATUS } from "../../../../../../src/constants";
 import { FilterFlexBox } from "../PaymentsContainer/style";
 import { AddressDetailsSkeleton } from "./AddressDetailsSkeleton";
 
+const initialValues = {
+  invoiceNumber: "",
+  orderId: "",
+  fromDate: "",
+  toDate: "",
+  status: "pending",
+};
+
 const SearchContainer = ({ path: string }) => {
   const dispatch = useDispatch();
-  const authUser = useSelector((state: any) => {
-    return state.auth?.user;
+
+  const [loading, setLoading] = useState<boolean>(true);
+  const [searchOrderData, setSearchOrderData] = useState<any>([]);
+  const [selectedRows, setSelectedRows] = useState<any>([]);
+  const [prevValues, setPrevValues] = useState<any>(initialValues);
+  const [pagination, setPagination] = useState({
+    count: 0,
+    page: 0,
+  });
+  const [sorting, setSorting] = useState({
+    field: "shippingDate",
+    type: "desc",
   });
 
-  const [searchRecordData, setSearchRecordData] = useState<any>();
-  const [selectedInvoiceId, setSelectedInvoiceId] = useState("");
+  const [loadingOrderDetails, setloadingOrderDetails] =
+    useState<boolean>(false);
   const [singleOrderData, setSingleOrderData] = useState([{}]);
+  const [selectedInvoiceId, setSelectedInvoiceId] = useState("");
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [drawerType, setDrawerType] = useState("");
-  const [loading, setLoading] = useState<boolean>(true);
-  const [totalPages, setTotalPages] = useState(1);
-  const [page, setPage] = useState(1);
-  const [totalData, setTotalData] = useState<any>(0);
-  const [sortType, setSortType] = useState<string | undefined>("desc");
-
-  const getSearchListData = async (values?: object) => {
-    console.log(values);
-    let urlParams = "";
-    if (values) {
-      values["fromDate"] = values["fromDate"]
-        ? moment(values["fromDate"]).format("YYYY-MM-DD")
-        : "";
-      values["toDate"] = values["toDate"]
-        ? moment(values["toDate"]).format("YYYY-MM-DD")
-        : "";
-      urlParams += "?";
-      let tempLen = Object.entries(values).length;
-      Object.entries(values).forEach(
-        ([key, value], index) =>
-          (urlParams += value
-            ? `${key}=${value}${index === tempLen - 1 ? "" : "&"}`
-            : "")
-      );
-    }
-    console.log(urlParams);
-    getSearchOrderListData(urlParams);
-  };
-
-  const getSearchOrderListData = async (url?: any) => {
-    setLoading(true);
-    const res = (await getSearchOrderList(url)) as any;
-    if (res.success) {
-      const orderList = res.response.data.data;
-      setSearchRecordData(orderList);
-      setPage(orderList?.pageMetaData?.page - 1);
-      setTotalPages(orderList?.pageMetaData?.totalPages);
-      setTotalData(orderList?.pageMetaData?.total);
-    } else if (!res.error) {
-      const InvoiceList = res;
-      setSearchRecordData(InvoiceList);
-    }
-    setLoading(false);
-  };
-
-  const getSearchPaginatedData = async (page, sortingField, sortingType) => {
-    let res;
-    setSortType(sortingType);
-    if (page === 0) {
-      res = (await getSearchOrderList('',0, 10, sortingField, sortingType)) as any;
-    }
-    else {
-      res = (await getSearchOrderList('',page+1, 10, sortingField, sortingType)) as any;
-    }
-    if (res.success) {
-      const orderList = res.response.data.data;
-      setPage(page);
-      setSearchRecordData(orderList);
-    }
-    else if (!res.error) {
-      const InvoiceList = res;
-      setSearchRecordData(InvoiceList);
-    }
-  };
-
-  const getSingleOrderData = async (id: any) => {
-    setLoading(true);
-    const res = (await getSearchOrderListById(id)) as any;
-    if (res.success) {
-      const orderListByID = res.response.data.data;
-      setSingleOrderData(orderListByID);
-    }
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    dispatch(singleActions.resetSingleShipment());
-    getSearchListData();
-  }, [dispatch]);
-
-  const tableTop = () => {
-    return (
-      <SearchTableTop>
-        <H3 text={`${totalData} Orders`} className="heading" />
-        <Button label="Print" onClick={() => {}} size="small" secondary />
-      </SearchTableTop>
-    );
-  };
-
-  const setData = (name, value) => {
-    if (name === "fromDate") {
-      setFieldValue("fromDate", moment(value).format("YYYY-MM-DD"));
-    } else if (name === "toDate") {
-      setFieldValue("toDate", moment(value).format("YYYY-MM-DD"));
-    }
-  };
-
-  const openInvoiceDrawer = (id: any, type: any) => {
-    if (type === "orderDetails") {
-      getSingleOrderData(id);
-      setSelectedInvoiceId(id);
-    } else if (type === "invoice") {
-      setSelectedInvoiceId(id);
-    }
-    setDrawerType(type);
-    setDrawerOpen(true);
-  };
 
   const getDrawerTitle = () => {
     if (drawerType === "invoice") {
@@ -162,10 +73,95 @@ const SearchContainer = ({ path: string }) => {
     }
   };
 
-  const applyOrderFilters = () => {
-    setDrawerType("");
-    setDrawerOpen(false);
-    getSearchListData();
+  const getSingleOrderData = async (id: any) => {
+    setloadingOrderDetails(true);
+    const res = (await getSearchOrderListById(id)) as any;
+    if (res.success) {
+      const orderListByID = res.response.data.data;
+      setSingleOrderData(orderListByID);
+    }
+    setloadingOrderDetails(false);
+  };
+
+  const openOrderDrawer = (id: any, type: any) => {
+    if (type === "orderDetails") {
+      getSingleOrderData(id);
+    }
+    setSelectedInvoiceId(id);
+    setDrawerType(type);
+    setDrawerOpen(true);
+  };
+
+  const tableTop = () => {
+    return (
+      <SearchTableTop>
+        <H3 text={`${pagination.count} Orders`} className="heading" />
+        <Button
+          label="Print"
+          onClick={() => {}}
+          size="small"
+          secondary
+          disabled
+        />
+      </SearchTableTop>
+    );
+  };
+
+  useEffect(() => {
+    dispatch(singleActions.resetSingleShipment());
+    getSearchOrderListData();
+  }, []);
+
+  const getSearchOrderListData = async (
+    values?: object,
+    page?: number,
+    sort?: { field: string; type: string }
+  ) => {
+    let urlParams = "",
+      rest = values !== undefined ? values : prevValues;
+    let params: any = {
+      ...rest,
+      page: page !== undefined ? page + 1 : pagination.page + 1,
+      chunk: 10,
+      sortingField: sort !== undefined ? sort.field : sorting.field,
+      sortingType: sort !== undefined ? sort.type : sorting.type,
+    };
+    if (values) {
+      setPrevValues(values);
+    } else {
+      resetForm({ values: prevValues });
+    }
+    if (sort) {
+      setSorting({
+        field: sort.field,
+        type: sort.type,
+      });
+    }
+    params["fromDate"] = params["fromDate"]
+      ? moment(params["fromDate"]).format("YYYY-MM-DD")
+      : "";
+    params["toDate"] = params["toDate"]
+      ? moment(params["toDate"]).format("YYYY-MM-DD")
+      : "";
+    let tempLen = Object.entries(params).length;
+    Object.entries(params).forEach(
+      ([key, value], index) =>
+        (urlParams += value
+          ? `${key}=${value}${index === tempLen - 1 ? "" : "&"}`
+          : "")
+    );
+    const res = (await getSearchOrderList(urlParams)) as any;
+    if (res.success) {
+      const data = res.response.data.data;
+      setSearchOrderData(data.list);
+      setPagination({
+        count: data.pageMetaData.total,
+        page: data.pageMetaData.page - 1,
+      });
+    } else {
+      setSearchOrderData([]);
+    }
+    setLoading(false);
   };
 
   const {
@@ -176,21 +172,17 @@ const SearchContainer = ({ path: string }) => {
     handleBlur,
     handleSubmit,
     setFieldValue,
+    resetForm,
   } = useFormik({
-    initialValues: {
-      invoiceNumber: "",
-      shippingId: "",
-      orderId: "",
-      fromDate: "",
-      toDate: "",
-      status: "pending",
-    },
-    onSubmit: (values) => getSearchListData(values),
+    initialValues,
+    onSubmit: (values) => getSearchOrderListData(values),
   });
 
-  if ([1, 2, 3, 4].indexOf(authUser?.roleId) === -1) {
-    navigate("/non-authorized-page");
-  }
+  const applyOrderFilters = () => {
+    setDrawerType("");
+    setDrawerOpen(false);
+    getSearchOrderListData();
+  };
 
   return (
     <ModuleContainer>
@@ -207,26 +199,29 @@ const SearchContainer = ({ path: string }) => {
               onChange={handleChange}
               error={touched.invoiceNumber && errors.invoiceNumber}
               label="Invoice Number"
-              placeholder="eg. 123,321"
+              placeholder="eg. 1234"
             />
           </Grid>
           <Grid item xs={12} sm={4} lg={2}>
-          {/* <Input label="Order Id" placeholder="eg. 123,321" /> */}
             <Input
-            id="orderId"
-            name="orderId"
-            initValue={values.orderId}
-            onBlur={handleBlur}
-            onChange={handleChange}
-            error={touched.orderId && errors.orderId}
-            label="Order Id"
-            placeholder="eg. 123,321"
-          />
-        </Grid>
+              id="orderId"
+              name="orderId"
+              initValue={values.orderId}
+              onBlur={handleBlur}
+              onChange={handleChange}
+              error={touched.orderId && errors.orderId}
+              label="Order Id"
+              placeholder="eg. 1234"
+            />
+          </Grid>
           <Grid item xs={6} sm={4} lg={2}>
             <DatePickerInput
               label="From Date"
-              maxDate={values.toDate ? new Date(moment(values.toDate).subtract(1,'days').toDate()): new Date()}
+              maxDate={
+                values.toDate
+                  ? moment(values.toDate).subtract(1, "days").toDate()
+                  : new Date()
+              }
               placeholder={"e.g 06/06/2021"}
               value={values.fromDate || null}
               onChange={(val) => setFieldValue("fromDate", val)}
@@ -234,11 +229,9 @@ const SearchContainer = ({ path: string }) => {
           </Grid>
           <Grid item xs={6} sm={4} lg={2}>
             <DatePickerInput
-              minDate={
-                new Date(moment(values.fromDate).add(1, "days").toDate())
-              }
-              maxDate={new Date()}
               label="To Date"
+              maxDate={new Date()}
+              minDate={moment(values.fromDate).add(1, "days").toDate()}
               placeholder={"e.g 06/06/2021"}
               value={values.toDate || null}
               onChange={(val) => setFieldValue("toDate", val)}
@@ -253,6 +246,7 @@ const SearchContainer = ({ path: string }) => {
               options={STATUS}
               value={values.status}
               onChange={handleChange}
+              allowEmpty
             />
           </Grid>
           <Grid item xs={6} sm={4} lg={2}>
@@ -260,7 +254,7 @@ const SearchContainer = ({ path: string }) => {
               <Button size="small" label="Search" onClick={handleSubmit} />
               <Box>
                 <img
-                  onClick={() => openInvoiceDrawer(null, "advanceFilter")}
+                  onClick={() => openOrderDrawer(null, "advanceFilter")}
                   src={sliders}
                   alt=""
                 />
@@ -272,18 +266,20 @@ const SearchContainer = ({ path: string }) => {
 
       {loading ? (
         <TableSkeleton />
-      ) : searchRecordData?.list?.length > 0 ? (
-        <Table
-          data={searchTable(searchRecordData?.list, openInvoiceDrawer)}
+      ) : searchOrderData.length > 0 ? (
+        <TableNew
           tableTop={tableTop()}
-          sortTypeProps = {sortType}
-          paginationData={(page, sortingField, sortingType) => getSearchPaginatedData(page, sortingField, sortingType)}
+          coloumns={searchOrderColoumns}
+          data={getSearchOrderData(searchOrderData, openOrderDrawer)}
           showCheckbox
+          onRowSelect={setSelectedRows}
           showPagination
-          page={page}
-          totalData={totalData}
-          totalPage={totalPages}
-          filterColumns={[0, 1, 2, 3, 4, 5]}
+          pagination={pagination}
+          onPageChange={(page) => getSearchOrderListData(undefined, page)}
+          sorting={sorting}
+          onSortChange={(field, type) =>
+            getSearchOrderListData(undefined, undefined, { field, type })
+          }
         />
       ) : (
         <NullState message="No Records Found" />
@@ -303,7 +299,7 @@ const SearchContainer = ({ path: string }) => {
           <AdvanceFilters applyFilters={applyOrderFilters} />
         ) : (
           <>
-            {loading ? (
+            {loadingOrderDetails ? (
               <AddressDetailsSkeleton />
             ) : (
               <SearchOrderDetailsDrawer
