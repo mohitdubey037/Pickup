2caa87c4f598080f7ebf53012b1fb1c8f697cc0d
torinit-darshan <darshan.sudake@torinit.ca>
2022-02-18 18:38:38 +0530

    On Hold Orders - Search, Sort, Pagination functionality

--- src/app/pages/DashboardPage/DashboardContainers/OnHoldShipment/OnHoldShipment.tsx
+++ src/app/pages/DashboardPage/DashboardContainers/OnHoldShipment/OnHoldShipment.tsx
@@ -1,284 +1,262 @@
-import { Button } from "app/components/Buttons";
-import { Input } from "app/components/Input";
-import ModuleContainer from "app/components/ModuleContainer";
-import { Table } from "app/components/Table";
-import { H2, H3 } from "app/components/Typography/Typography";
-import { sliders } from "app/assets/Icons";
-import { useCallback, useEffect, useState } from "react";
-import { getHoldingShipmentsService, scheduleShipmentService, deleteShipmentService } from "services/HoldingService";
-
+import { useEffect, useState } from "react";
+import moment from "moment";
+import { useDispatch } from "react-redux";
+import { useFormik } from "formik";
 import { Box } from "@mui/material";
 import { Grid } from "@material-ui/core";
+
+import { Button } from "app/components/Buttons";
+import { Input } from "app/components/Input";
+import { Flex } from "app/components/Input/style";
+import ModuleContainer from "app/components/ModuleContainer";
+import { TableNew } from "app/components/Table";
+import { H2, H3, H5 } from "app/components/Typography/Typography";
+import { sliders } from "app/assets/Icons";
 import { Drawer } from "app/components/Drawer";
-import OrderDetailsDrawer from "../SignleShipmentContainer/OrderDetailsDrawer";
-import { getOrderIdListFromIndexList, onHoldTable } from "./helper";
-import ScheduleShipmentsDrawer from "./ScheduleShipmentsDrawer";
-import { showToast } from "utils";
-import { FilterFlexBox } from "../PaymentsContainer/style";
 import DatePickerInput from "app/components/Input/DatePickerInput";
 import { GridContainer } from "app/components/GridSpacing/GridSpacing";
+import TableSkeleton from "app/components/Table/TableSkeleton";
+import NullState from "app/components/NullState/NullState";
+import { getHoldingShipmentsService } from "services/HoldingService";
+import { actions as singleActions } from "store/reducers/SingleShipmentReducer";
+import OrderDetailsDrawer from "../SignleShipmentContainer/OrderDetailsDrawer";
+import { onHoldOrderColoumns, getOnHoldOrderData } from "./helper";
+import ScheduleShipmentsDrawer from "./ScheduleShipmentsDrawer";
+import { FilterFlexBox } from "../PaymentsContainer/style";
 import { SearchTableTop } from "../SearchContainer/style";
 
-export interface OnHoldDataType {
-    category: string;
-    invoiceId: string;
-    invoiceNumber: string;
-    itemCount: number;
-    orderId: string;
-    shipmentCost: number;
-    shippingDate: string;
-    shippingReference: string;
-    status: string;
-    total: string;
-    type: string;
-}
-
-interface FilterType {
-    invoiceNumber: string;
-    shippingId: string;
-    fromDate: string;
-    toDate: string;
-}
+const initialValues = {
+  shippingId: "",
+  fromDate: "",
+  toDate: "",
+};
 
 const OnHoldShipmentContainer = ({ path: string }) => {
+  const dispatch = useDispatch();
 
-    const initialFilter = {
-        invoiceNumber: "",
-        shippingId: "",
-        fromDate: "",
-        toDate: ""
+  const [loading, setLoading] = useState<boolean>(true);
+  const [onHoldOrderData, setOnHoldOrderData] = useState<any>([]);
+  const [selectedRows, setSelectedRows] = useState<any>([]);
+  const [prevValues, setPrevValues] = useState<any>(initialValues);
+  const [pagination, setPagination] = useState({
+    count: 0,
+    page: 0,
+  });
+  const [sorting, setSorting] = useState({
+    field: "shippingDate",
+    type: "desc",
+  });
+
+  const [selectedOrderId, setSelectedOrderId] = useState("");
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [drawerType, setDrawerType] = useState("");
+
+  const getDrawerTitle = () => {
+    if (drawerType === "orderDetails") {
+      return "Order Items";
+    } else if (drawerType === "scheduleOrder") {
+      return "Schedule Orders";
+    } else {
+      return "";
     }
+  };
 
-    const [onHoldData, setHoldData] = useState<OnHoldDataType[]>([])
-    const [filters, setFilters] = useState<FilterType>(initialFilter)
-    const [fromDateOpen, setFromDateOpen] = useState<boolean>(false)
-    const [toDateOpen, setToDateOpen] = useState<boolean>(false)
-    const [drawerOpen, setDrawerOpen] = useState<boolean>(false)
-    const [singleScheduleDrawerOpen, setScheduleDrawerOpen] = useState<boolean>(false)
-    const [orderId, setOrderId] = useState<string>("")
-    const [selectedIndex, setSelectedIndex] = useState<number[]>([])
+  const openOnHoldDrawer = (id: any, type: any) => {
+    setSelectedOrderId(id);
+    setDrawerType(type);
+    setDrawerOpen(true);
+  };
 
-    const getOnHoldDataHandler = async () => {
-        const res: { response: any, error: any } = await getHoldingShipmentsService(filters)
-            if (res.response) {
-                setHoldData(res.response.data.data)
-        }
-    }
-
-    const deleteShipmentHandler = async () => {
-        const res: { response: any, error: any } = await deleteShipmentService(orderId)
-            if (res.response) {
-                showToast(`Order ${orderId} ${res.response.data.message}`, "success")
-                // setHoldData(res.response.data.data)
-        }
-    }
-
-    const getOnHoldDatahandlerCallback = useCallback(getOnHoldDataHandler, [filters])
-
-    useEffect(() => {
-        getOnHoldDatahandlerCallback()
-    }, [getOnHoldDatahandlerCallback]);
-
-    const onChangeHandler = (e, type, val) => {
-        const updatedFilter = { ...filters }
-        if (type === "invoiceNumber") updatedFilter.invoiceNumber = e.target.value
-        if (type === "shippingId") updatedFilter.shippingId = e.target.value
-        if (type === "fromDate") updatedFilter.fromDate = val
-        if (type === "toDate") updatedFilter.toDate = val
-        setFilters(updatedFilter)
-    }
-
-    const openInvoiceDrawer = (id: string) => {
-        setOrderId(id);
-        setDrawerOpen(true);
-    }
-
-    const tableTop = () => {
-        return (
-            <SearchTableTop>
-                <H3 text={`${onHoldData.length} Orders`} className="heading" />
-                    <Box>
-                    <Button label="Delete" secondary={true} onClick={() => deleteShipmentHandler()} size="medium" style={{marginRight:'12px'}} />
-                    <Button label="Schedule" onClick={() => setScheduleDrawerOpen(true)}  size="medium"  />
-                    </Box>
-            </SearchTableTop>
-        );
-    };
-
-    const handleSubmitHandler = async (shippingSchedule: string, date: string | null, time: string | null) => {
-        const orderIdList = await getOrderIdListFromIndexList(onHoldData, selectedIndex)
-        console.log('orderIdList', orderIdList)
-        const data = {
-            scheduleType: shippingSchedule,
-            orderAt: date,
-            shipment: orderIdList
-        }
-        const res: { response: any, error: any } = await scheduleShipmentService(data)
-        if(!res.error){
-            setSelectedIndex([])
-
-        }
-    }
-
-    const singleScheduleHandler = (id: string, index: number) => {
-        setOrderId(id);
-        setSelectedIndex([index]);
-        setScheduleDrawerOpen(true);
-    }
-
-    const multipleScheduleHandler = (values: any) => {
-        setSelectedIndex([...values]);
-    }
-
+  const tableTop = () => {
     return (
-        <ModuleContainer>
-            <H2 title="On Hold Shipments" />
-            {/* <Grid container spacing={4} alignItems="center" style={{ marginTop: "20px", marginBottom: "20px" }}>
-                <Grid item xs={2}>
-                    <Input label="Invoice Number" value={filters.invoiceNumber} onChange={(e) => onChangeHandler(e, 'invoiceNumber', null)} placeholder="eg. 123,321" />
-                </Grid>
-                <Grid item xs={2}>
-                    <Input label="Order Id" value={filters.shippingId} onChange={(e) => onChangeHandler(e, 'shippingId', null)} placeholder="eg. 123,321" />
-                </Grid>
-                <Grid item xs={2}>
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <DatePicker
-                            label="From Order Date"
-                            value={filters.fromDate || null}
-                            onChange={(val) => onChangeHandler(null, "fromDate", val)}
-                            open={fromDateOpen}
-                            onOpen={() => setFromDateOpen(true)}
-                            onClose={() => setFromDateOpen(false)}
-                            renderInput={(params) => (
-                                <TextField
-                                    label="Date"
-                                    placeholder={"e.g 06/06/2021"}
-                                    {...params}
-                                    onClick={() => setFromDateOpen(true)}
-                                    defaultValue={""}
-                                />
-                            )}
-                        />
-                    </LocalizationProvider>
-                </Grid>
-                <Grid item xs={2}>
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <DatePicker
-                            label="To Order Date"
-                            value={filters.toDate || null}
-                            onChange={(val) => onChangeHandler(null, "toDate", val)}
-                            open={toDateOpen}
-                            onOpen={() => setToDateOpen(true)}
-                            onClose={() => setToDateOpen(false)}
-                            renderInput={(params) => (
-                                <TextField
-                                    label="Date"
-                                    placeholder={"e.g 06/06/2021"}
-                                    {...params}
-                                    onClick={() => setToDateOpen(true)}
-                                    defaultValue={""}
-                                />
-                            )}
-                        />
-                    </LocalizationProvider>
-                </Grid>
-                <Grid item xs={2}>
-                    <img src={sliders} alt="" />
-                </Grid>
-            </Grid> */}
+      <SearchTableTop>
+        <Flex alignItems="center">
+          <H3 text={`${pagination.count} Orders`} className="heading" />
+          <H5
+            text={`(${selectedRows.length} Selected)`}
+            className="spanlabel"
+          />
+        </Flex>
+        <Box>
+          <Button
+            size="medium"
+            label="Delete"
+            secondary={true}
+            style={{ marginRight: "12px" }}
+          />
+          <Button size="medium" label="Schedule" />
+        </Box>
+      </SearchTableTop>
+    );
+  };
 
+  useEffect(() => {
+    dispatch(singleActions.resetSingleShipment());
+    getOnHoldOrderListData();
+  }, []);
 
-            <Box mt={4} mb={2}>
+  const getOnHoldOrderListData = async (
+    values?: object,
+    page?: number,
+    sort?: { field: string; type: string }
+  ) => {
+    let urlParams = "",
+      rest = values !== undefined ? values : prevValues;
+    let params: any = {
+      ...rest,
+      page: page !== undefined ? page + 1 : pagination.page + 1,
+      chunk: 10,
+      sortingField: sort !== undefined ? sort.field : sorting.field,
+      sortingType: sort !== undefined ? sort.type : sorting.type,
+    };
+    if (values) {
+      setPrevValues(values);
+    } else {
+      resetForm({ values: prevValues });
+    }
+    if (sort) {
+      setSorting({
+        field: sort.field,
+        type: sort.type,
+      });
+    }
+    params["fromDate"] = params["fromDate"]
+      ? moment(params["fromDate"]).format("YYYY-MM-DD")
+      : "";
+    params["toDate"] = params["toDate"]
+      ? moment(params["toDate"]).format("YYYY-MM-DD")
+      : "";
+    let tempLen = Object.entries(params).length;
+    Object.entries(params).forEach(
+      ([key, value], index) =>
+        (urlParams += value
+          ? `${key}=${value}${index === tempLen - 1 ? "" : "&"}`
+          : "")
+    );
+    const res = (await getHoldingShipmentsService(urlParams)) as any;
+    console.log(res);
+    if (res.error === null) {
+      const data = res.response.data.data;
+      setOnHoldOrderData(data.list);
+      setPagination({
+        count: data.pageMetaData.total,
+        page: data.pageMetaData.page - 1,
+      });
+    } else {
+      setOnHoldOrderData([]);
+    }
+    setLoading(false);
+  };
+
+  const {
+    values,
+    handleChange,
+    errors,
+    touched,
+    handleBlur,
+    handleSubmit,
+    setFieldValue,
+    resetForm,
+  } = useFormik({
+    initialValues,
+    onSubmit: (values) => getOnHoldOrderListData(values),
+  });
+
+  return (
+    <ModuleContainer>
+      <H2 title="On Hold Orders" />
+
+      <Box mt={4} mb={2}>
         <GridContainer container spacing={2}>
-          <Grid item xs={6} sm={4} lg={2}>
-            <Input
-              id="invoiceNumber"
-              name="invoiceNumber"
-              onChange={(e) => onChangeHandler(e, 'invoiceNumber', null)} 
-              label="Invoice Number"
-              placeholder="eg. 1234"
-            />
-          </Grid>
           <Grid item xs={12} sm={4} lg={2}>
             <Input
-              id="orderId"
-              name="orderId"
-              onChange={(e) => onChangeHandler(e, 'shippingId', null)} 
+              id="shippingId"
+              name="shippingId"
+              initValue={values.shippingId}
+              onBlur={handleBlur}
+              onChange={handleChange}
+              error={touched.shippingId && errors.shippingId}
               label="Order Id"
               placeholder="eg. 1234"
             />
           </Grid>
           <Grid item xs={6} sm={4} lg={2}>
             <DatePickerInput
-              label="From Order Date"
+              label="From Date"
+              maxDate={
+                values.toDate
+                  ? moment(values.toDate).subtract(1, "days").toDate()
+                  : new Date()
+              }
               placeholder={"e.g 06/06/2021"}
-              value={filters.fromDate || null}
-              onChange={(val) => onChangeHandler(null, "fromDate", val)}
+              value={values.fromDate || null}
+              onChange={(val) => setFieldValue("fromDate", val)}
             />
           </Grid>
           <Grid item xs={6} sm={4} lg={2}>
             <DatePickerInput
-              label="To Order Date"
+              label="To Date"
               maxDate={new Date()}
+              minDate={moment(values.fromDate).add(1, "days").toDate()}
               placeholder={"e.g 06/06/2021"}
-              value={filters.toDate || null}
-              onChange={(val) => onChangeHandler(null, "toDate", val)}
+              value={values.toDate || null}
+              onChange={(val) => setFieldValue("toDate", val)}
             />
           </Grid>
-       
           <Grid item xs={12} sm={8} lg={4}>
-          <FilterFlexBox>
-              <Button size="small" label="Search" />
+            <FilterFlexBox>
+              <Button size="small" label="Search" onClick={handleSubmit} />
               <Box>
-                <img
-                  src={sliders}
-                  alt=""
-                />
+                <img src={sliders} alt="" />
               </Box>
             </FilterFlexBox>
           </Grid>
         </GridContainer>
       </Box>
 
+      {loading ? (
+        <TableSkeleton />
+      ) : onHoldOrderData.length > 0 ? (
+        <TableNew
+          tableTop={tableTop()}
+          coloumns={onHoldOrderColoumns}
+          data={getOnHoldOrderData(onHoldOrderData, openOnHoldDrawer)}
+          showCheckbox
+          onRowSelect={setSelectedRows}
+          showPagination
+          pagination={pagination}
+          onPageChange={(page) => getOnHoldOrderListData(undefined, page)}
+          sorting={sorting}
+          onSortChange={(field, type) =>
+            getOnHoldOrderListData(undefined, undefined, { field, type })
+          }
+        />
+      ) : (
+        <NullState message="No Records Found" />
+      )}
 
-
-
-            <Table
-                data={onHoldTable(onHoldData, openInvoiceDrawer, singleScheduleHandler)}
-                tableTop={tableTop()}
-                showCheckbox
-                showPagination
-                perPageRows={10}
-                filterColumns={[0, 1, 2, 3, 4, 5]}
-                getSelectedItems={multipleScheduleHandler}
-                selectedItems={selectedIndex}
-            />
-
-            <Drawer
-                open={drawerOpen}
-                title={"Order Items"}
-                setDrawerOpen={(flag) => setDrawerOpen(flag)}
-                closeIcon={true}
-                actionButtons={true}
-            >
-                <OrderDetailsDrawer orderId={orderId} setDrawerOpen={setDrawerOpen} />
-            </Drawer>
-            
-            <Drawer
-                open={singleScheduleDrawerOpen}
-                title={"Schedule Shipments"}
-                setDrawerOpen={(flag) => setScheduleDrawerOpen(flag)}
-                closeIcon={true}
-                actionButtons={true}
-            >
-                <ScheduleShipmentsDrawer 
-                handleSubmit={handleSubmitHandler} 
-                submitButtonLabel={"Save"} 
-                setDrawerOpen={setScheduleDrawerOpen} />
-            </Drawer>
-
-        </ModuleContainer>
-    );
+      <Drawer
+        open={drawerOpen}
+        title={getDrawerTitle()}
+        setDrawerOpen={(flag) => setDrawerOpen(flag)}
+        closeIcon={true}
+        actionButtons={true}
+      >
+        {drawerType === "orderDetails" ? (
+          <OrderDetailsDrawer
+            orderId={selectedOrderId}
+            setDrawerOpen={setDrawerOpen}
+          />
+        ) : (
+          <ScheduleShipmentsDrawer
+            handleSubmit={() => {}}
+            setDrawerOpen={setDrawerOpen}
+            submitButtonLabel={"Save"}
+          />
+        )}
+      </Drawer>
+    </ModuleContainer>
+  );
 };
 
 export default OnHoldShipmentContainer;
