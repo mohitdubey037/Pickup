0d266ee777da629251c4b853104507c512385e2a
tushar9sd <tushar@torinit.ca>
2022-02-23 19:40:25 +0530

    issue resolved

---
+++ src/app/pages/DashboardPage/DashboardContainers/ChildAccount/style.ts
@@ -0,0 +1,15 @@
+import { FullCard } from "app/components/Input/style";
+import styled from "styled-components";
+
+export const CreateChildAccountBox = styled(FullCard)`
+ display:flex;
+ flex-direction:column;
+ height:300px;
+ justify-content:center;
+ align-items:center;
+ gap: 16px;
+ .label{
+     color:#A6A6A6;
+     margin:0;
+ }
+`;
